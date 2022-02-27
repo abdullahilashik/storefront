9dbc71e44294c2ecadc363befd43925e3e76932b
Ashik Arefin <abdullahilashik@gmail.com>
2022-02-27 08:30:24 +0600

    settings

---
+++ store/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.extfield()
+    price  = models.DecimalField(max_digits=6 ,decimal_places=2)
+    inventory = models.IntegerField(default=1)
+    last_update = models.DateTimeField(auto_now=True)
+    
